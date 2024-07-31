a6e5fead346e60df3c35622288402a61905681fc
m-anshu <mohananshuj@gmail.com>
2024-07-31 10:47:11 +0530

    Added settings for Reinforcement Learning

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import random
 import gym
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layer import Dense,Flatten
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
 
 env =gym.make("CartPole-v1",render_mode="human")
+
+states = env.observation_space.shape[0] #possible states in current env
+actions = env.action_space.n #number of various actions that can be performed in the current env
+
+
 
 episodes = 10
 for episode in range(1,episodes+1):
